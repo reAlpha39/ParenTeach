42229dc51f7d6dfe76f87ad295255464edb10a65
AdiatsaPutra <santikosetyo77@gmail.com>
2021-05-09 08:19:19 +0700

    Initialize firebase on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> init = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return FutureBuilder(
+      future: init,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error goes here');
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyHomePage();
+        }
+        return CircularProgressIndicator();
+      },
     );
   }
 }
