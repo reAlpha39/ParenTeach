1f53995a27326afe99e103330cd94028ef762ef2
AdiatsaPutra <santikosetyo77@gmail.com>
2021-05-19 11:37:03 +0700

    add playground_illustration

--- lib/views/playground_page.dart
+++ lib/views/playground_page.dart
@@ -10,50 +10,67 @@ class PlaygroundPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                width: Get.width,
-                height: 350,
-                decoration: BoxDecoration(
-                  color: pinkColor,
-                  image: DecorationImage(
-                      image: AssetImage('assets/playground.png'),
-                      fit: BoxFit.cover),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20, vertical: margin),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Get.back();
-                        },
-                        child: Container(
-                          height: 30,
-                          width: 30,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Icon(
-                            Icons.arrow_back,
-                            color: pinkColor,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        'Activities',
-                        style: whiteTextBold.copyWith(fontSize: 24),
-                      ),
-                      Icon(
-                        Icons.notifications,
-                        color: Colors.white,
-                        size: 30,
-                      )
-                    ],
+              Stack(
+                children: [
+                  Container(
+                    width: Get.width,
+                    height: 400,
+                    decoration: BoxDecoration(
+                      color: pinkColor,
+                      image: DecorationImage(
+                          image: AssetImage('assets/playground.png'),
+                          fit: BoxFit.cover),
+                    ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: margin),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                Get.back();
+                              },
+                              child: Container(
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Icon(
+                                  Icons.arrow_back,
+                                  color: pinkColor,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Activities',
+                              style: whiteTextBold.copyWith(fontSize: 24),
+                            ),
+                            Icon(
+                              Icons.notifications,
+                              color: Colors.white,
+                              size: 30,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 250,
+                        ),
+                        Text(
+                          'Ayo Bermain',
+                          style: whiteText.copyWith(fontSize: 18),
+                        ),
+                        //TODO: Buat Permainan
+                      ],
+                    ),
+                  ),
+                ],
               )
             ],
           ),
