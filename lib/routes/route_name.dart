b252dbf6923c7ad2aeb4cb71eabc03a709e3fe1a
ReAlpha39 <yunusw718@gmail.com>
2021-06-08 21:27:55 +0800

    add enum route name

---
+++ lib/routes/route_name.dart
@@ -0,0 +1,20 @@
+enum RouteName { LOGINPAGE, HOMEPAGE }
+
+final routeName = EnumValues({
+  'login_page': RouteName.LOGINPAGE,
+  '/home_page': RouteName.HOMEPAGE
+});
+
+class EnumValues<T> {
+  Map<String, T>? map;
+  Map<T, String>? reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    if (reverseMap == null) {
+      reverseMap = map!.map((k, v) => new MapEntry(v, k));
+    }
+    return reverseMap!;
+  }
+}
