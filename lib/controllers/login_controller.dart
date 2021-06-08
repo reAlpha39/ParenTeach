006455697d6d6f2e2acacab4eb15c49e049c7759
ReAlpha39 <yunusw718@gmail.com>
2021-06-09 00:06:05 +0800

    show dialog when error happen

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:parenteach/repositories/database_provider.dart';
 import 'package:parenteach/routes/route_name.dart';
+import 'package:parenteach/utils/utils.dart';
 
 class LoginController extends GetxController {
   final DatabaseProvider _databaseProvider = DatabaseProvider();
@@ -29,10 +30,31 @@ class LoginController extends GetxController {
 
   void initializeFirestore() async {
     isLoading.value = true;
-    await _databaseProvider.initializeFirebase();
-    isLoading.value = false;
-    Get.offAndToNamed(
-      routeName.reverse[RouteName.HOMEPAGE]!,
+    try {
+      await _databaseProvider.initializeFirebase();
+      isLoading.value = false;
+      Get.offAndToNamed(
+        routeName.reverse[RouteName.HOMEPAGE]!,
+      );
+    } catch (e) {
+      _showDialog(title: 'Error', middleText: "Error: " + e.toString());
+    }
+  }
+
+  _showDialog({required String title, required String middleText}) {
+    Get.defaultDialog(
+      barrierDismissible: false,
+      titleStyle: blackText.copyWith(fontSize: 24),
+      middleTextStyle: blackText.copyWith(fontSize: 18),
+      title: title,
+      middleText: middleText,
+      textConfirm: 'OK',
+      radius: 17,
+      buttonColor: pinkColor,
+      confirmTextColor: Colors.white,
+      onConfirm: () {
+        Navigator.of(Get.overlayContext!).pop();
+      },
     );
   }
 }
