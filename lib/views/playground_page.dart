3c2781006e723875875e68b2e6023136e9065e3d
ReAlpha39 <yunusw718@gmail.com>
2021-06-09 11:06:14 +0800

    add playground page interface

--- lib/views/playground_page.dart
+++ lib/views/playground_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parenteach/routes/route_name.dart';
+import 'package:parenteach/utils/utils.dart';
+import 'package:parenteach/widgets/playground_card.dart';
 
 class PlaygroundPage extends StatelessWidget {
   const PlaygroundPage({Key? key}) : super(key: key);
@@ -6,8 +10,110 @@ class PlaygroundPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add_alert_rounded),
+            ),
+          ),
+        ],
+        centerTitle: true,
+        title: Text(
+          'Playground',
+          style: whiteTextBold.copyWith(fontSize: 24),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: Get.width,
+            color: pinkColor,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: Get.width,
+                  child: Image.asset(
+                    'assets/playground.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 72,
+                      left: 32,
+                    ),
+                    child: Text(
+                      'Ayo Bermain Bunda',
+                      style: whiteText.copyWith(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding: const EdgeInsets.only(
+              top: 280,
+              left: 32,
+              right: 32,
+            ),
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      PlaygroundCard(
+                        route: RouteName.HOMEPAGE,
+                        titleCard: 'Melempar Bola',
+                        imageAsset: 'assets/beach_ball_icon.png',
+                      ),
+                      PlaygroundCard(
+                        route: RouteName.HOMEPAGE,
+                        titleCard: 'Bermain Puzzle',
+                        imageAsset: 'assets/puzzle_icon.png',
+                      ),
+                      PlaygroundCard(
+                        route: RouteName.HOMEPAGE,
+                        titleCard: 'Barbel',
+                        imageAsset: 'assets/barbell_icon.png',
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      PlaygroundCard(
+                        route: RouteName.HOMEPAGE,
+                        titleCard: 'Memasak',
+                        imageAsset: 'assets/soup_icon.png',
+                      ),
+                      PlaygroundCard(
+                        route: RouteName.HOMEPAGE,
+                        titleCard: 'Bersepeda',
+                        imageAsset: 'assets/bicycle_icon.png',
+                      ),
+                      PlaygroundCard(
+                        route: RouteName.HOMEPAGE,
+                        titleCard: 'Bermain Catur',
+                        imageAsset: 'assets/chess_icon.png',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/playground_card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parenteach/routes/route_name.dart';
+import 'package:parenteach/utils/theme.dart';
+
+class PlaygroundCard extends StatelessWidget {
+  final String imageAsset;
+  final String titleCard;
+  final RouteName route;
+
+  const PlaygroundCard({
+    Key? key,
+    required this.imageAsset,
+    required this.titleCard,
+    required this.route,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Get.offAndToNamed(routeName.reverse[route]!),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 12,
+                  right: 12,
+                  top: 8,
+                ),
+                child: Image.asset(imageAsset),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  titleCard,
+                  style: blackText,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
