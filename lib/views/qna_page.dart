4af91f3d86086d5cd022add33bad5f19724f3be4
ReAlpha39 <yunusw718@gmail.com>
2021-06-10 17:28:36 +0800

    add qna page interface

--- lib/views/qna_page.dart
+++ lib/views/qna_page.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:parenteach/utils/utils.dart';
 import 'package:parenteach/widgets/custom_appbar.dart';
+import 'package:parenteach/widgets/qna_card.dart';
 
 class QnaPage extends StatelessWidget {
   const QnaPage({Key? key}) : super(key: key);
+  static List<String> dummyText = [
+    'Bagaimana membangun sifat anak yang baik?',
+    'Sikap yang baik itu seperti apa?',
+    'Apa peran orang tua agar anak dapat berkelakuan baik?',
+    'Bagaimana membangun sifat anak yang baik?',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +22,132 @@ class QnaPage extends StatelessWidget {
         enableNotifIcon: false,
         backgroundColor: pinkColor,
         foregroundColor: Colors.white,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: Get.width,
+            height: 160,
+            child: Container(
+              color: pinkColor,
+              alignment: Alignment.topCenter,
+              padding: const EdgeInsets.only(
+                top: 12,
+              ),
+              width: 200,
+              child: Image.asset(
+                'assets/qna.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 135,
+              left: 32,
+              right: 32,
+            ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 14,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: Icon(
+                          Icons.search_rounded,
+                          color: pinkColor,
+                        ),
+                      ),
+                      Container(
+                        width: 250,
+                        height: 20,
+                        child: TextField(
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Cari pertanyaan disini',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Color(0xff909090),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding: const EdgeInsets.only(
+              top: 190,
+              left: 32,
+              right: 32,
+            ),
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: List<Widget>.generate(
+                  dummyText.length,
+                  (index) => QnaCard(
+                    titleCard: dummyText[index],
+                  ),
+                )
+                  ..insert(
+                    0,
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Sikap',
+                        style: pinkText.copyWith(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
+                  ..add(
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Bakat Siswa',
+                        style: pinkText.copyWith(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
+                  ..addAll([
+                    QnaCard(
+                      titleCard: dummyText[0],
+                    ),
+                    QnaCard(
+                      titleCard: dummyText[1],
+                    ),
+                    QnaCard(
+                      titleCard: dummyText[2],
+                    ),
+                  ]),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/widgets/qna_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parenteach/routes/route_name.dart';
+import 'package:parenteach/utils/theme.dart';
+
+class QnaCard extends StatelessWidget {
+  final String titleCard;
+
+  const QnaCard({
+    Key? key,
+    required this.titleCard,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          width: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+          ),
+          child: Row(
+            children: [
+              Flexible(
+                child: Text(
+                  titleCard,
+                  style: blackTextBold.copyWith(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Text(
+                  '5\nJawaban',
+                  textAlign: TextAlign.center,
+                  style: pinkText.copyWith(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
