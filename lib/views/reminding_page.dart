e7b257bbb6c2dff7b8abfc5b3dcdccdd941d042a
AdiatsaPutra <santikosetyo77@gmail.com>
2021-06-19 10:47:35 +0700

    Fixing reminding page layout

--- lib/views/reminding_page.dart
+++ lib/views/reminding_page.dart
@@ -12,17 +12,41 @@ class RemindingPage extends StatelessWidget {
       body: SafeArea(
         child: Stack(
           children: [
-            Container(
-              height: 250,
-              width: Get.width,
-              decoration: BoxDecoration(color: pinkColor),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Image.asset(
-                  'assets/reminding_illustration.png',
-                  fit: BoxFit.cover,
+            Stack(
+              children: [
+                Container(
+                  height: 250,
+                  width: Get.width,
+                  decoration: BoxDecoration(color: pinkColor),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Image.asset(
+                      'assets/reminding_illustration.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: InkWell(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: pinkColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
@@ -63,22 +87,22 @@ class RemindingPage extends StatelessWidget {
                       ],
                     ),
                   ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    'Pilih Remindingmu',
-                    style: blackText,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
                   Expanded(
                     child: SingleChildScrollView(
                       physics: BouncingScrollPhysics(),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Lakukan Reminding Berikut',
+                            style: blackTextBold,
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
                           PilihRemindingCard(),
                           PilihRemindingCard(),
                           PilihRemindingCard(),
