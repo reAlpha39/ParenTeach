fd6ba1c71481017d54d51022cbe95b2f0f5ab61b
AdiatsaPutra <santikosetyo77@gmail.com>
2021-06-19 10:43:32 +0700

    Change reminding page layout

--- lib/views/reminding_page.dart
+++ lib/views/reminding_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import '../utils/theme.dart';
-import '../widgets/favorite_reminding_card.dart';
-import '../widgets/pilih_reminding_card.dart';
-import '../widgets/reminding_page_textfield.dart';
+import 'package:parenteach/utils/utils.dart';
+import 'package:parenteach/widgets/favorite_reminding_card.dart';
+import 'package:parenteach/widgets/pilih_reminding_card.dart';
+import 'package:parenteach/widgets/reminding_page_textfield.dart';
 
 class RemindingPage extends StatelessWidget {
   @override
@@ -27,71 +26,81 @@ class RemindingPage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: SingleChildScrollView(
-                physics: BouncingScrollPhysics(),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 24,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 170,
+                  ),
+                  Text(
+                    'Reminding',
+                    style: whiteTextBold.copyWith(fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 70,
+                    width: Get.width,
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        'Reminding Page',
-                        style: whiteTextBold.copyWith(fontSize: 24),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 70,
-                    ),
-                    Text(
-                      'Reminding Favorite',
-                      style: whiteText.copyWith(fontSize: 18),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        FavoriteRemindingCard(),
-                        FavoriteRemindingCard(),
+                        Text(
+                          'Apakah sudah melakukannya?',
+                          style: blackText.copyWith(
+                              fontSize: 14, color: Colors.grey),
+                        ),
+                        Text(
+                          'Miftah',
+                          style: blackTextBold.copyWith(
+                              fontSize: 18, color: pinkColor),
+                        ),
                       ],
                     ),
-                    SizedBox(
-                      height: 15,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Pilih Remindingmu',
+                    style: blackText,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                          PilihRemindingCard(),
+                        ],
+                      ),
                     ),
-                    RemindingPageTextField(),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      'Pilih Remindingmu',
-                      style: blackText,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                    PilihRemindingCard(),
-                  ],
-                ),
+                  ),
+                ],
               ),
             )
           ],
