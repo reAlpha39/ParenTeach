7223708d89d291e74448cc7c9ff8afb0b481ffdf
AdiatsaPutra <santikosetyo77@gmail.com>
2021-06-19 11:59:24 +0700

    Added Presensi and Sikap Siswa

--- lib/views/rapot_page.dart
+++ lib/views/rapot_page.dart
@@ -1,10 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parenteach/utils/theme.dart';
 
 class RapotPage extends StatelessWidget {
-  const RapotPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              height: 170,
+              width: Get.width,
+              decoration: BoxDecoration(
+                color: pinkColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(24, 20, 24, 0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Get.back();
+                        },
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: pinkColor,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Raport',
+                        style: whiteTextBold.copyWith(fontSize: 24),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  // TODO: Add TabBar To Change Raport Type
+                  SizedBox(
+                    height: 130,
+                  ),
+                  Container(
+                    width: Get.width,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              'Presensi Siswa',
+                              style: blackText,
+                            ),
+                            Text(
+                              '90 %',
+                              style: blackTextBold,
+                            ),
+                          ],
+                        ),
+                        Container(
+                          color: Colors.grey.withOpacity(0.3),
+                          height: 70,
+                          width: 1,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              'Sikap siswa',
+                              style: blackText,
+                            ),
+                            Text(
+                              'BAIK',
+                              style: blackTextBold,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      buildKategoriRapot(blueColor, 'Pengetahuan'),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      buildKategoriRapot(Colors.yellow, 'Keterampilan'),
+                    ],
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: List.generate(
+                          10,
+                          (index) => Container(
+                            margin: EdgeInsets.only(
+                              top: 10,
+                            ),
+                            width: Get.width,
+                            height: 55,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildKategoriRapot(Color color, String kategori) {
+    return Row(
+      children: [
+        Container(
+          height: 13,
+          width: 13,
+          decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Text(
+          kategori,
+          style: blackTextBold,
+        ),
+      ],
+    );
   }
 }
