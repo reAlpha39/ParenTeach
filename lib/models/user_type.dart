450a6d23dca1e29ef65e1459d0fdb9ddb94977d7
ReAlpha39 <yunusw718@gmail.com>
2021-07-26 12:04:00 +0800

    admin, orang_tua, and guru inherits users

---
+++ lib/models/user_type.dart
@@ -0,0 +1,9 @@
+import 'package:parenteach/utils/utils.dart';
+
+enum UserType { ORANGTUA, ADMIN, GURU }
+
+final userTypeValues = EnumValues({
+  "orangtua": UserType.ORANGTUA,
+  "admin": UserType.ADMIN,
+  "guru": UserType.GURU
+});
---
+++ lib/models/users.dart
@@ -0,0 +1,51 @@
+// To parse this JSON data, do
+//
+//     final users = usersFromMap(jsonString);
+
+import 'dart:convert';
+
+import 'package:parenteach/models/user_type.dart';
+
+class Users {
+  Users({
+    this.idUsers,
+    this.usersname,
+    this.password,
+    this.nama,
+    this.noHp,
+    this.email,
+    this.type,
+  });
+
+  String? idUsers;
+  String? usersname;
+  String? password;
+  String? nama;
+  String? noHp;
+  String? email;
+  UserType? type;
+
+  factory Users.fromJson(String str) => Users.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Users.fromMap(Map<String, dynamic> json) => Users(
+        idUsers: json["idUsers"] == null ? null : json["idUsers"],
+        usersname: json["usersname"] == null ? null : json["usersname"],
+        password: json["password"] == null ? null : json["password"],
+        nama: json["nama"] == null ? null : json["nama"],
+        noHp: json["noHp"] == null ? null : json["noHp"],
+        email: json["email"] == null ? null : json["email"],
+        type: json["type"] == null ? null : userTypeValues.map![json["type"]],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "idUsers": idUsers == null ? null : idUsers,
+        "usersname": usersname == null ? null : usersname,
+        "password": password == null ? null : password,
+        "nama": nama == null ? null : nama,
+        "noHp": noHp == null ? null : noHp,
+        "email": email == null ? null : email,
+        "type": type == null ? null : userTypeValues.reverse[type],
+      };
+}
