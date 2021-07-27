f17ce241c9c71678f5ef1b14c2b296dc018fcabb
AdiatsaPutra <santikosetyo77@gmail.com>
2021-07-27 11:45:20 +0700

    Add tambah nilai rapot page

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:parenteach/views/admin/admin_home_page.dart';
 import 'package:parenteach/views/admin/admin_qna_page.dart';
 import 'package:parenteach/views/admin/admin_reminding_page.dart';
 import 'package:parenteach/views/admin/admin_tambah_guru.dart';
+import 'package:parenteach/views/admin/admin_tambah_nilai_rapot.dart';
 import 'package:parenteach/views/admin/daftar_guru.dart';
 import 'package:parenteach/views/admin/daftar_siswa.dart';
 import '../views/rapot_page.dart';
@@ -80,6 +81,10 @@ class Routes {
     GetPage(
       name: routeName.reverse[RouteName.ADMINTAMBAHGURUPAGE]!,
       page: () => AdminTambahGuru(),
+    ),
+    GetPage(
+      name: routeName.reverse[RouteName.ADMINTAMBAHNILAIRAPOTPAGE]!,
+      page: () => AdminTambahNilaiRapot(),
     ),
   ];
 }
