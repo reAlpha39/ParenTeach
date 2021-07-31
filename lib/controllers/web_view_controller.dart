5575c93eddeef331c3194e234e4cbb82791526de
AdiatsaPutra <santikosetyo77@gmail.com>
2021-07-31 21:55:47 +0700

    added progress indicator to webview

--- lib/controllers/web_view_controller.dart
+++ lib/controllers/web_view_controller.dart
@@ -4,14 +4,10 @@ import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebviewController extends GetxController {
+  RxBool isLoading = true.obs;
   @override
   void onInit() {
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
     super.onInit();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
   }
 }
--- lib/views/web_view_page.dart
+++ lib/views/web_view_page.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:parenteach/controllers/web_view_controller.dart';
+import 'package:parenteach/utils/theme.dart';
+import 'package:parenteach/widgets/custom_appbar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebviewPage extends StatelessWidget {
   final WebviewController webviewController = Get.find();
-
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      initialUrl: Get.arguments,
-      javascriptMode: JavascriptMode.unrestricted,
+    return Scaffold(
+      appBar: CustomAppBar(
+          enableLeading: true,
+          title: '',
+          backgroundColor: pinkColor,
+          foregroundColor: greyBackgroundColor),
+      body: Obx(
+        () => Stack(
+          children: [
+            WebView(
+              initialUrl: Get.arguments,
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageFinished: (finish) {
+                webviewController.isLoading.value = false;
+              },
+            ),
+            webviewController.isLoading.value
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Stack(),
+          ],
+        ),
+      ),
     );
   }
 }
